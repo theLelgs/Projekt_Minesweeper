127c840e8347239cc7a55265b0b902314ba62954
Linus Lindström <linus@datasnillet.com>
2025-11-19 12:08:40 +0100

    Bytt från ASCII till Raylib

--- Projekt_Minesweeper/Program.cs
+++ Projekt_Minesweeper/Program.cs
@@ -1,86 +1,68 @@
 ﻿using System.Numerics;
 using System.Text;
 using Raylib_cs;
-//The symbols used by the map when displaying them. 
-List<char> Symbols = [' ', '1', '2', '3', '4', '5', '6', '7', '8', '۞', '?'];
-List<(int X,int Y)> SurroundingSquares= [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)];
+
+//What every Item equals to:
+//0 = Empty
+//1-8 = 1-8
+//9 = Mine
+//10 = Flag
+//11 = Unknown
+
 
 //To allow for more characters to be used in the console
 //looked this up tho
-Console.OutputEncoding = Encoding.UTF8;
-// Raylib.InitWindow(800,600,"MineSweeper");
-// Raylib.SetTargetFPS(60);
-// Raylib.ToggleBorderlessWindowed();
-
-
-
-
-Dictionary<string, (int MaxX, int MaxY, int Bombs)> MapSizes = [];
-MapSizes["Small"] = (10, 10, 15);
-MapSizes["Medium"] = (20, 15, 50);
-MapSizes["Large"] = (25, 25, 100);
-
-
-Console.WriteLine("Choose your map size: ");
-int linecount = 0;
-foreach (string MapSize in MapSizes.Keys) //Writes out the different map options
+// Console.OutputEncoding = Encoding.UTF8;
+Raylib.InitWindow(25,25,"Temporary");
+int monitor = Raylib.GetCurrentMonitor();
+int MaxWidth = Raylib.GetMonitorWidth(monitor);
+int MaxHeight = Raylib.GetMonitorHeight(monitor);
+Raylib.CloseWindow();
+Console.Clear();
+Console.WriteLine($"Choose a map width. Maximum width is {MaxWidth/25}, and the minimum is 10");
+int width;
+int height;
+int Minecount;
+string widthString = Console.ReadLine();
+while (!int.TryParse(widthString, out width)||width>MaxWidth/25||width<10)
 {
-    linecount++;
-    (int X, int Y, int mines) = MapSizes[MapSize];
-    Console.WriteLine($"{linecount}. {MapSize}: {X}x{Y}. Minecount: {mines}");
+    widthString = Console.ReadLine();
 }
-Console.WriteLine("4. Custom");
-
-ConsoleKeyInfo Input = Console.ReadKey(true);
-ConsoleKey[] InputList = [ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3, ConsoleKey.D4];
-while (!InputList.Contains(Input.Key))
+Console.WriteLine($"Choose a map height. Maximum width is {MaxHeight/25}, and the minimum is 1");
+string heightString = Console.ReadLine();
+while (!int.TryParse(heightString, out height)||height>MaxHeight/25||height<1)
 {
-    Input = Console.ReadKey(true);
+    heightString = Console.ReadLine();
 }
-if (Input.Key == ConsoleKey.D4)//Allows the user to make a custom board
+Console.WriteLine($"Choose a number of mines. Maximum is {width*height} and the minimum is 1");
+string Minestring = Console.ReadLine();
+while (!int.TryParse(Minestring, out Minecount)||Minecount<1)
 {
-    int Width;
-    int Height;
-    int Mines;
-    Console.Clear();
-    Console.WriteLine("How wide do you want the board to be?");
-    string tempWidth = Console.ReadLine();
-    while (!int.TryParse(tempWidth, out Width)||Width<=0||Width>120)
-    {
-        Console.Clear();
-        Console.WriteLine("How wide do you want the board to be?");
-        tempWidth = Console.ReadLine();
-    }
-    Console.Clear();
-    Console.WriteLine("How tall do you want the board to be?");
-    string tempHeight = Console.ReadLine();
-    while (!int.TryParse(tempHeight, out Height)||Height<=0||Height>30)
-    {
-        Console.Clear();
-        Console.WriteLine("How tall do you want the board to be?");
-        tempHeight = Console.ReadLine();
-    }
-    Console.Clear();
-    Console.WriteLine("How many mines do you want there to be?");
-    string tempMines = Console.ReadLine();
-    while (!int.TryParse(tempMines, out Mines) || Mines > Height * Width)
-    {
-        Console.Clear();
-        Console.WriteLine("How many mines do you want there to be?");
-        tempMines = Console.ReadLine();
-    }
-    Console.Clear();
-    MapSizes["Custom"] = (Width, Height, Mines);
-
+    Minestring=Console.ReadLine();
 }
-string[] MapSizeKeyList = ["Small", "Medium", "Large", "Custom"];
-(int MaxX, int MaxY, int Minecount) = MapSizes[MapSizeKeyList[int.Parse(Input.KeyChar.ToString()) - 1]];
+
+
+Raylib.InitWindow(25*width,25*height,"MineSweeper");
+Raylib.SetTargetFPS(60);
+int MaxX = width;
+int MaxY=height;
+
+
 int[,] Map = new int[MaxX, MaxY];
+int[,] PlayerKnownMap = new int[MaxX, MaxY];
+for (int y = 0;y<MaxY;y++)
+{
+    for (int x = 0; x<MaxX;x++)
+    {
+        PlayerKnownMap[x,y]=11;
+    }
+}
 
 List<(int, int)> FlagPositions = [];
 List<(int,int)> SquaresRevealed = [];
+List<(int,int)> SurroundingSquares= [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)];
 
-
+Texture2D[] Sprites = [Raylib.LoadTexture("img/SquareZero.png"),Raylib.LoadTexture("img/SquareOne.png"), Raylib.LoadTexture("img/SquareTwo.png"),Raylib.LoadTexture("img/SquareThree.png"), Raylib.LoadTexture("imt/SquareFour.png"), Raylib.LoadTexture("img/SquareFive.png"), Raylib.LoadTexture("img/SquareSix.png"), Raylib.LoadTexture("img/SquareSeven.png"), Raylib.LoadTexture("img/SquareEight.png"), Raylib.LoadTexture("img/SquareMine.png"), Raylib.LoadTexture("img/SquareFlag.png"), Raylib.LoadTexture("img/SquareUnknown.png")];
 
 for (int a = 0; a < Minecount; a++) //Adds mines equal to the Minecount integer. No duplicates
     {
@@ -95,112 +77,85 @@ for (int a = 0; a < Minecount; a++) //Adds mines equal to the Minecount integer.
         Map[RandomX, RandomY] = 9;
 
     }
-
-Console.Clear();
-WriteMap(MaxX, MaxY);
-Console.ReadKey(true);
-
-for (int a = 0;a<MaxX;a++)
-{
-    for (int b = 0; b < MaxY; b++)
-    {
-        Console.CursorLeft = a;
-        Console.CursorTop = b;
-        Console.Write(Symbols[10]);
-    }
-}
-
 Vector2 CursorPos = new(MaxX / 2, MaxY / 2);
-Console.CursorLeft = (int)CursorPos.X;
-Console.CursorTop = (int)CursorPos.Y;
-
-
 bool playing = true;
-// while(!Raylib.WindowShouldClose())
-
-// Raylib.BeginDrawing();
-// Raylib.ClearBackground(Color.SkyBlue);
-while(playing&&Input.Key!=ConsoleKey.Escape)
+while(!Raylib.WindowShouldClose()&&playing)
 {
-    Input = Console.ReadKey(true);
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.SkyBlue);
+    List<Color> colors = [Color.DarkBrown, Color.Blue, Color.Gray, Color.Gold, Color.Magenta, Color.Lime, Color.Brown, Color.Brown, Color.Brown, Color.Red, Color.Brown, Color.Brown, Color.Brown, Color.Brown, Color.Brown, Color.Brown];
+    for (int x = 0;x<MaxX;x++)
+    {
+        for (int y = 0;y<MaxY;y++)
+        {
+            Raylib.DrawTexture(Sprites[PlayerKnownMap[x,y]],x*25,y*25, Color.White);
+        }
+    }
+    Raylib.DrawRectangleLines((int)CursorPos.X*25, (int)CursorPos.Y*25, 25, 25, Color.Red);
     //Moves the cursor on WASD input
-    if (Input.Key==ConsoleKey.W&&CursorPos.Y>0)
-        {
-            CursorPos -= new Vector2(0, 1);
-        }
-    if (Input.Key==ConsoleKey.A&&CursorPos.X>0)
-        {
-            CursorPos -= new Vector2(1, 0);
-        }
-    if (Input.Key==ConsoleKey.S&&CursorPos.Y<MaxY-1)
-        {
-            CursorPos += new Vector2(0, 1);
-        }
-    if (Input.Key == ConsoleKey.D&&CursorPos.X<MaxX-1)
-        {
-            CursorPos += new Vector2(1, 0);
-        }
-    if (Input.Key == ConsoleKey.Enter&&!FlagPositions.Contains(((int,int))(CursorPos.X,CursorPos.Y)))    //Looks for mines in the surrounding 8 tiles on enter press
-
-        {
-            RevealSquare(Map, (int)CursorPos.X, (int)CursorPos.Y);
-        }
-    if (Input.Key==ConsoleKey.Spacebar&&!SquaresRevealed.Contains(((int,int))(CursorPos.X,CursorPos.Y)))
+    if (Raylib.IsKeyPressed(KeyboardKey.W)&&CursorPos.Y>0)
+            {
+                CursorPos -= new Vector2(0, 1);
+            }
+    if (Raylib.IsKeyPressed(KeyboardKey.A)&&CursorPos.X>0)
+            {
+                CursorPos -= new Vector2(1, 0);
+            }
+    if (Raylib.IsKeyPressed(KeyboardKey.S)&&CursorPos.Y<MaxY-1)
+            {
+                CursorPos += new Vector2(0, 1);
+            }
+    if (Raylib.IsKeyPressed(KeyboardKey.D)&&CursorPos.X<MaxX-1)
+            {
+                CursorPos += new Vector2(1, 0);
+            }
+    if (Raylib.IsKeyPressed(KeyboardKey.Enter)&&PlayerKnownMap[(int)CursorPos.X,(int)CursorPos.Y]!=10)    //Looks for mines in the surrounding 8 tiles on enter press
     {
-        if (!FlagPositions.Contains(((int, int))(CursorPos.X, CursorPos.Y)))
-        {
-            Console.Write("T");
-            FlagPositions.Add(((int, int))(CursorPos.X, CursorPos.Y));
-        }
-        else
-        {
-            Console.Write("?");
-            FlagPositions.Remove(((int, int))(CursorPos.X, CursorPos.Y));
-        }
-        Console.CursorTop=0;
-        Console.CursorLeft=MaxX+13;
-        Console.Write(Minecount-FlagPositions.Count+"     ");
+        RevealSquare(Map, (int)CursorPos.X, (int)CursorPos.Y);
     }
-    Console.CursorLeft = (int)CursorPos.X; //Moves the cursor to it's correct position
-    Console.CursorTop = (int)CursorPos.Y;
-    
-    if (SquaresRevealed.Count+FlagPositions.Count>=MaxX*MaxY&&FlagPositions.Count==Minecount) //Check if player has won
+    if (Raylib.IsKeyPressed(KeyboardKey.Space))
     {
-        playing=false;
+        if (PlayerKnownMap[(int)CursorPos.X,(int)CursorPos.Y]==11)
+        {
+            PlayerKnownMap[(int)CursorPos.X,(int)CursorPos.Y]=10;
+            FlagPositions.Add(((int,int))(CursorPos.X, CursorPos.Y));
+        }
+        else if (PlayerKnownMap[(int)CursorPos.X,(int)CursorPos.Y]==10)
+        {
+            PlayerKnownMap[(int)CursorPos.X,(int)CursorPos.Y]=11;
+            FlagPositions.Remove(((int,int))(CursorPos.X, CursorPos.Y));
+        }
     }
+        if (SquaresRevealed.Count+FlagPositions.Count>=MaxX*MaxY&&FlagPositions.Count==Minecount) //Check if player has won
+        {
+            playing=false;
+        }
+        Raylib.EndDrawing(); 
 }
-// Raylib.EndDrawing();
-
-Console.Clear();
 if (SquaresRevealed.Count+FlagPositions.Count>=MaxX*MaxY)
 {
-    Console.WriteLine("You Win!");
+    //WIN
 }
 else
 {
-    Console.WriteLine("You Lose!");
+    //LOSE
 }
-Console.ReadKey(true);
 
 void RevealSquare(int[,] Map, int XPos, int YPos)
 {
-    Console.CursorLeft=XPos;
-    Console.CursorTop=YPos;
-    Console.Write(Symbols[MineDetection(Map, XPos,YPos)]);
     if (!SquaresRevealed.Contains((XPos, YPos))&&MineDetection(Map,XPos,YPos)!=9)//Dont add to SquaresRevealed if it already contains it
     {
     SquaresRevealed.Add((XPos,YPos));
     }
+    PlayerKnownMap[XPos,YPos]=MineDetection(Map,XPos,YPos);
     if (MineDetection(Map, XPos, YPos)==0)//Reveal all surrounding squares, rerunning RevealSquare on neighbouring 0
     {
         foreach ((int,int) PositionMod in SurroundingSquares)
         {
             if (!(XPos+PositionMod.Item1>MaxX-1||XPos+PositionMod.Item1<0||YPos+PositionMod.Item2>MaxY-1||YPos+PositionMod.Item2<0)&&!SquaresRevealed.Contains((XPos+PositionMod.Item1,YPos+PositionMod.Item2)))
             {
-                Console.CursorLeft=XPos+PositionMod.Item1;
-                Console.CursorTop=YPos+PositionMod.Item2;
                 SquaresRevealed.Add((XPos+PositionMod.Item1,YPos+PositionMod.Item2));
-                Console.Write(Symbols[MineDetection(Map, XPos+PositionMod.Item1, YPos+PositionMod.Item2)]);
+                PlayerKnownMap[XPos+PositionMod.Item1,YPos+PositionMod.Item2]=MineDetection(Map, XPos+PositionMod.Item1,YPos+PositionMod.Item2);
                 if (MineDetection(Map, XPos+PositionMod.Item1, YPos+PositionMod.Item2)==0)
                 {
                     RevealSquare(Map,XPos+PositionMod.Item1,YPos+PositionMod.Item2);
@@ -211,7 +166,6 @@ void RevealSquare(int[,] Map, int XPos, int YPos)
     if (MineDetection(Map, XPos, YPos)==9)//Detect if player revealed a mine
     {
         playing=false;
-        Console.ReadKey(true);
     }
 }
 int MineDetection(int[,] Map, int XPos, int YPos)
@@ -232,30 +186,4 @@ int MineDetection(int[,] Map, int XPos, int YPos)
         }
     }
     return output;
-}
-void WriteMap(int MaxX, int MaxY) //Writes out the map
-{
-    for (int y = 0; y < MaxY; y++)
-    {
-        for (int x = 0; x < MaxX; x++)
-        {
-            Console.Write("?");
-            if (x == MaxX - 1)
-            {
-                Console.Write("|");
-                if (y==0)
-                {
-                    Console.Write($"Bombs left: {Minecount}");
-                }
-                Console.Write("\n");
-            }
-        }
-        if (y == MaxY-1)
-        {
-            for (int x = 0; x < MaxX; x++)
-            {
-                Console.Write("=");
-            }
-        }
-    }
 }
